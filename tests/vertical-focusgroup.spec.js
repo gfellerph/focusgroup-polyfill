b864657ac39d3abf5ea37bfa6af3a2e9dc519b40
Philipp Gfeller <philipp.gfeller@post.ch>
2023-12-11 10:06:31 +0100

    add some more tests

---
+++ tests/vertical-focusgroup.spec.js
@@ -0,0 +1,36 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
+
+test("Vertical focusgroup arrow right", async ({ page }) => {
+  await page.locator("#vertical-focusgroup-button-1").press("ArrowRight");
+  await expect(page.locator("#vertical-focusgroup-button-2")).toBeFocused();
+});
+
+test("Vertical focusgroup arrow down", async ({ page }) => {
+  await page.locator("#vertical-focusgroup-button-1").press("ArrowDown");
+  await expect(page.locator("#vertical-focusgroup-button-2")).toBeFocused();
+});
+
+test("Vertical focusgroup arrow left", async ({ page }) => {
+  await page.locator("#vertical-focusgroup-button-2").press("ArrowLeft");
+  await expect(page.locator("#vertical-focusgroup-button-2")).toBeFocused();
+});
+
+test("Vertical focusgroup arrow up", async ({ page }) => {
+  await page.locator("#vertical-focusgroup-button-2").press("ArrowUp");
+  await expect(page.locator("#vertical-focusgroup-button-1")).toBeFocused();
+});
+
+test("Vertical focusgroup does not wrap right", async ({ page }) => {
+  await page.locator("#vertical-focusgroup-button-5").press("ArrowDown");
+  await expect(page.locator("#vertical-focusgroup-button-5")).toBeFocused();
+});
+
+test("Vertical focusgroup does not wrap left", async ({ page }) => {
+  await page.locator("#vertical-focusgroup-button-1").press("ArrowUp");
+  await expect(page.locator("#vertical-focusgroup-button-1")).toBeFocused();
+});
---
+++ tests/wrong-hv-focusgroup.spec.js
@@ -0,0 +1,40 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
+
+test("Wrong horizontal vertical focusgroup arrow right", async ({ page }) => {
+  await page.locator("#wrong-hv-focusgroup-button-1").press("ArrowRight");
+  await expect(page.locator("#wrong-hv-focusgroup-button-2")).toBeFocused();
+});
+
+test("Wrong horizontal vertical focusgroup arrow down", async ({ page }) => {
+  await page.locator("#wrong-hv-focusgroup-button-1").press("ArrowDown");
+  await expect(page.locator("#wrong-hv-focusgroup-button-2")).toBeFocused();
+});
+
+test("Wrong horizontal vertical focusgroup arrow left", async ({ page }) => {
+  await page.locator("#wrong-hv-focusgroup-button-2").press("ArrowLeft");
+  await expect(page.locator("#wrong-hv-focusgroup-button-1")).toBeFocused();
+});
+
+test("Wrong horizontal vertical focusgroup arrow up", async ({ page }) => {
+  await page.locator("#wrong-hv-focusgroup-button-2").press("ArrowUp");
+  await expect(page.locator("#wrong-hv-focusgroup-button-1")).toBeFocused();
+});
+
+test("Wrong horizontal vertical focusgroup does not wrap right", async ({
+  page,
+}) => {
+  await page.locator("#wrong-hv-focusgroup-button-5").press("ArrowRight");
+  await expect(page.locator("#wrong-hv-focusgroup-button-5")).toBeFocused();
+});
+
+test("Wrong horizontal vertical focusgroup does not wrap left", async ({
+  page,
+}) => {
+  await page.locator("#wrong-hv-focusgroup-button-1").press("ArrowLeft");
+  await expect(page.locator("#wrong-hv-focusgroup-button-1")).toBeFocused();
+});
